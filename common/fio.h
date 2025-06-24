e44c4c7f7e8a482ed7a152b0e0bbdf6eb45cab5a
Hank Lewis <hank@modernatomic.com>
2025-06-24 09:05:36 +0200

    added fio.h

---
+++ common/fio.h
@@ -0,0 +1,32 @@
+#ifndef __FIO_H__
+#define __FIO_H__
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "log.h"
+
+static inline const char* read_file_contents(const char* path) {
+    FILE* stream;
+    stream = fopen(path, "rb");
+    if(!stream) LOG_AND_CLOSE(stream, "Couldn't find source file [%s].", path);
+    if(fseek(stream, 0L, SEEK_END)) 
+        LOG_AND_CLOSE(stream, "Seeking to EOF failed [%s].", path);
+    long temp = ftell(stream);
+    if(temp < 0) LOG_AND_CLOSE(stream, "Invalid file size [%s].", path);
+    size_t file_size = (size_t) temp;
+    if(fseek(stream, 0L, SEEK_SET))
+        LOG_AND_CLOSE(stream, "Seeking to beginning of file failed [%s].", path);
+    char* contents;
+    contents = (char*) malloc(file_size + 1);
+    if(!(contents = (char*) malloc(file_size + 1)))
+        LOG_AND_CLOSE(stream, "Unable to allocate buffer for file contents [%s].", path)
+    size_t size = fread(contents,1,file_size, stream);
+    if(size < file_size && !feof(stream))
+        LOG_AND_CLOSE(stream, "Read less bytes than expected [%s].", path);
+    contents[size] = '\0';
+    fclose(stream);
+    return contents;
+}
+
+#endif // __FIO_H__
--- common/log.h
+++ common/log.h
@@ -2,8 +2,6 @@
 #define __LOG_H__
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <assert.h>
 
 #define LOG_INFO 0
@@ -11,19 +9,25 @@
 
 #ifdef LOGGING
 //
-#define LOG(lvl, fmt, ...) {\
+#define LOG(lvl, ...) {\
     FILE* stream = ((lvl) == LOG_ERROR) ? stderr : stdout;\
     fprintf(stream, "%s [%s:%d]: ", ((lvl) == LOG_ERROR) ? "ERROR" : "INFO", __FILE__, __LINE__);\
-    fprintf(stream, fmt, __VA_ARGS__);\
+    fprintf(stream, __VA_ARGS__);\
     fprintf(stream, "\n");\
+}
+#define LOG_AND_CLOSE(stream, ...) {\
+    LOG(LOG_ERROR, __VA_ARGS__);\
+    fclose((stream));\
+    return NULL;\
 }
 //
 #define ASSERT(cond) for(; !(cond); assert(cond), cond = cond)
-#define ASSERT_LOG(cond, fmt, ...) ASSERT(cond) LOG(LOG_ERROR, fmt, __VA_ARGS__);
+#define ASSERT_LOG(cond, ...) ASSERT(cond) LOG(LOG_ERROR, __VA_ARGS__);
 //
 #else
 //
 #define LOG(lvl, fmt, ...)
+#define LOG_AND_CLOSE(stream, fmt, ...)
 //
 #define ASSERT(cond)
 #define ASSERT_LOG(cond, fmt, ...)
